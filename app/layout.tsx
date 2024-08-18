60bd8bc64d3cb0326a62b9fdc9ce0de3a2ae3d03
rohanarun <rohanarun@gmail.com>
2024-08-18 00:37:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import 'styles/main.css';
 
 const title = 'Next.js Subscription Starter';
 const description = 'Brought to you by Vercel, Stripe, and Supabase.';
+  const web_html = process.env.APP_NAME;
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
@@ -22,18 +23,8 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className="bg-black">
-        <Navbar />
-        <main
-          id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
-        >
-          {children}
-        </main>
-        <Footer />
-        <Suspense>
-          <Toaster />
-        </Suspense>
+      <body className="bg-black"  dangerouslySetInnerHTML={{ __html: web_html }} >
+    
       </body>
     </html>
   );
