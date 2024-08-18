fdde449233679dd398f416d19a76615b036bd102
rohanarun <rohanarun@gmail.com>
2024-08-18 14:24:48 -0400

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,7 @@
 import CustomerPortalForm from '@/components/ui/AccountForms/CustomerPortalForm';
 import EmailForm from '@/components/ui/AccountForms/EmailForm';
+import Pricing from '@/components/ui/Pricing/Pricing';
+
 import NameForm from '@/components/ui/AccountForms/NameForm';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
@@ -25,11 +27,12 @@ export default async function Account() {
     <section className="mb-32 bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
         <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Account
-          </h1>
+      <Pricing
+      user={user}
+      products={products ?? []}
+      subscription={subscription}
+    />
           <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
           </p>
         </div>
       </div>
